2e90eab8043499e017740a90019a809294bc2988
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-11-12 18:39:32 +0100

    ejercicio2 hecho, funciona

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
     let pokemon = pokeapi.results.find(dato => dato.name.toLowerCase() === fugitivo )
     
     if(!pokemon){
-      datosPokemon.innerHTML = `<p style= "color:red;">Pokemon ${fugitivo} no encontrado`
+      datosPokemon.innerHTML = `<p style= "color:red;">Pokemon ${fugitivo} no encontrado</p>`
     }
     try{
 
@@ -67,8 +67,46 @@ function buscarSinAsync(){
       .then((respuesta) => respuesta.json())
       .then((pokeapi) => {
           let pokemon = pokeapi.results.find(dato => dato.name.toLowerCase() === fugitivo )
-           console.log(pokemon) 
+          
+        if(!pokemon){
+          datosPokemon.innerHTML = `<p style= "color:red;">Pokemon ${fugitivo} no encontrado</p>`
+          return
+        }
+
+        return fetch(pokemon.url)
+
+          .then((segundaRespuesta) => segundaRespuesta.json())
+          .then((datos) => {
+                const {base_experience, height, weight} = datos
+                const urlFront= datos.sprites.front_default
+                const urlBack = datos.sprites.back_default
+                const grito = datos.cries.legacy
+
+                datosPokemon.innerHTML= `
+                      <h3>${pokemon.name.toUpperCase()}</h3>
+
+                      <img src="${urlFront}" style="width:200px; height: 200px">
+                      <img src="${urlBack}" style="width:200px; height: 200px">
+                      <p><strong>Experiencia base:</strong> ${base_experience}</p>
+                      <p><strong>Altura:</strong> ${height} cm</p>
+                      <p><strong>Peso:</strong> ${weight} kg</p>
+                      <button id="play" style= "width:100px; height:50px; background-color:yellow; border:1px solid yellow; border-radius:5px;">Sonido</button>
+                       <audio id="audio">
+                          <source src="${grito}" type="audio/mpeg">
+                          Tu navegador no soporta el elemento de audio.
+                        </audio>`
+                  
+                const audio = document.getElementById('audio')
+                const sonido = document.getElementById('play')
+                sonido.addEventListener('click', () =>{
+                    audio.play()
+                })
+          })
       })
-      
-    
+
+     
+
+      .catch(error => {
+        console.log(error)
+      })
 }
